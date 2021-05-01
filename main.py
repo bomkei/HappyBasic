b2c224ec58a5f030f83516b8a74af0f5a7f4d50b
kumitatepazuru <riku.ueda@outlook.jp>
2021-05-02 06:14:02 +0900

    バグ修正&returnコード表示

--- main.py
+++ main.py
@@ -11,33 +11,33 @@ class msg(commands.Cog):
         self.bot = bot
         self.logger = logging.getLogger(__name__)
 
-    @commands.command(prefix="bb")
+    @commands.command(aliases=["bb"])
     async def buildbasic(self, ctx):
-        msg = "BUILD FROM BASIC LANG IN C++"
-        m: discord.Message = await ctx.send(msg)
-        p = subprocess.Popen(["make"],
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
-        _ = await process_output(p, m, msg, ctx)
-        await ctx.send("ENDED.")
-
-    @commands.command(prefix="db")
-    async def debugbasic(self, ctx):
-        msg = "DEBUG FROM BASIC LANG IN C++"
+        msg = "BUILD FROM BASIC LANG IN C++\n"
         m: discord.Message = await ctx.send(msg)
         p = subprocess.Popen(["make","-C","./plugin/BASIC_lang/"],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
         _ = await process_output(p, m, msg, ctx)
         await ctx.send("ENDED.")
-        msg = "chmod a+x ./basic"
+
+    @commands.command(aliases=["db"])
+    async def debugbasic(self, ctx):
+        msg = "DEBUG FROM BASIC LANG IN C++\n"
+        m: discord.Message = await ctx.send(msg)
+        p = subprocess.Popen(["make","-C","./plugin/BASIC_lang/"],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+        _ = await process_output(p, m, msg, ctx)
+        await ctx.send("ENDED.")
+        msg = "chmod a+x ./basic\n"
         m: discord.Message = await ctx.send(msg)
         p = subprocess.Popen(["chmod","a+x","./plugin/BASIC_lang/basic"],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
         _ = await process_output(p, m, msg, ctx)
-        await ctx.send("ENDED.")
-        msg = "RUN DEBUG"
+        await ctx.send("ENDED. CODE:"+str(p.returncode))
+        msg = "RUN DEBUG\n"
         m: discord.Message = await ctx.send(msg)
         p = subprocess.Popen(["./plugin/BASIC_lang/basic"],
                              stdout=subprocess.PIPE,
