bcef932757fae913d19084b9266e8482bf0f8703
zkr5471 <mrzkr2ds@gmail.com>
2021-05-02 01:52:58 +0900

    move source to utils.cpp

--- BASIC_lang/utils.cpp
+++ BASIC_lang/utils.cpp
@@ -1,5 +1,27 @@
 #include "main.h"
 
+// 文字色指定 for Windows Console
+void SetConsoleColor(int col)
+{
+  HANDLE hCons = GetStdHandle(STD_OUTPUT_HANDLE);    // コンソールハンドル取得
+  WORD attr = 0;                                     // API に渡すアトリビュート
+
+  if( col & COL_INTENSITY )        // 高輝度ビットが立っていれば
+    attr |= FOREGROUND_INTENSITY;  // アトリビュートの高輝度ビットを立てる
+
+  if( col & COL_RED_MASK )
+    attr |= FOREGROUND_RED;
+
+  if( col & COL_GREEN_MASK )
+    attr |= FOREGROUND_GREEN;
+
+  if( col & COL_BLUE_MASK )
+    attr |= FOREGROUND_BLUE;
+
+  // 色指定
+  SetConsoleTextAttribute(hCons, attr);
+}
+
 bool is_hex(std::string str)
 {
   for( auto&& c : str )
