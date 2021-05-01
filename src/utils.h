c01d5787eabb6f0192f9a0b11073f7a4f60d1b5d
zkr5471 <mrzkr2ds@gmail.com>
2021-05-02 06:16:06 +0900

    insert spaces

--- src/utils.h
+++ src/utils.h
@@ -3,9 +3,9 @@
 template <class T, class F, class... Args>
 int find_vector(std::vector<T>& vec, F compare, Args ...args)
 {
-  for(int i=0;i<vec.size();i++)
-   if( compare(vec[i], args...) )
-     return i;
+  for( int i = 0; i < vec.size(); i++ )
+    if( compare(vec[i], args...) )
+      return i;
 
   return -1;
 }
