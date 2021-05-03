bb1f238f871eea1f97722295060dd0b49d76f6c3
zkr5471 <mrzkr2ds@gmail.com>
2021-05-03 20:24:39 +0900

    move function implementation to .cpp

--- src/include/Object.h
+++ src/include/Object.h
@@ -21,90 +21,10 @@ struct Object
 
   Object* var_ptr = nullptr;
 
-  Object& operator = (Object const& obj)
-  {
-    type = obj.type;
-    v_int = obj.v_int;
-    v_float = obj.v_float;
-    v_char = obj.v_char;
+  Object& operator = (Object const& obj);
 
-    list = obj.list;
-    var_ptr = obj.var_ptr;
-
-    return *this;
-  }
-
-  std::string to_string() const
-  {
-    switch( type )
-    {
-    case Int:
-      return std::to_string(v_int);
-
-    case Float:
-      return std::to_string(v_float);
-
-    case Char:
-      return std::string(1, v_char);
-
-    case Array:
-    {
-      std::string s = "";
-
-      if( is_string() )
-      {
-        for( auto&& c : list )
-          s += c.v_char;
-
-        return s;
-      }
-
-      for( int i = 0; i < list.size(); i++ )
-      {
-        s += list[i].to_string();
-        if( i < list.size() - 1 ) s += ", ";
-      }
-
-      return  "[" + s + "]";
-    }
-
-    case Pointer:
-      return "{" + var_ptr->name + "}";
-    }
-
-    return "";
-  }
-
-  bool is_string() const
-  {
-    if( type != Array )
-      return false;
-
-    for( auto&& c : list )
-      if( c.type != Char )
-        return false;
-
-    return list.size() != 0;
-  }
-
-  bool eval() const
-  {
-    switch( type )
-    {
-    case Int:
-      return v_int != 0;
-
-    case Float:
-      return v_float != 0;
-
-    case Char:
-      return v_char != 0;
-
-    case Array:
-      return list.size() != 0;
-    }
-
-    return false;
-  }
+  std::string to_string() const;
+  bool is_string() const;
+  bool eval() const;
 };
 
