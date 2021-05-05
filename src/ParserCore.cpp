f9723af686bf7a0317a8c40c9d05c0f1e09d550d
zkr5471 <mrzkr2ds@gmail.com>
2021-05-06 08:24:19 +0900

    impl parse of While

--- src/ParserCore.cpp
+++ src/ParserCore.cpp
@@ -396,6 +396,42 @@ AST::Stmt* ParserCore::Stmt()
   }
 
   //
+  // while
+  if( consume("while") )
+  {
+    auto tk = csmtok;
+
+    auto cond = Expr();
+    expect("\n");
+
+    std::vector<AST::Stmt*> block;
+    auto closed = false;
+
+    while( check() )
+    {
+      if( consume("wend") )
+      {
+        expect("\n");
+        closed = true;
+        break;
+      }
+
+      block.emplace_back(Stmt());
+    }
+
+    if( !closed )
+      Program::Error(*tk, "not closed");
+    
+    auto ast = new AST::While;
+    ast->token = tk;
+    ast->cond = cond;
+    ast->code = new AST::Block;
+    ast->code->list = std::move(block);
+
+    return ast;
+  }
+
+  //
   // def
   if( consume("def") )
   {
