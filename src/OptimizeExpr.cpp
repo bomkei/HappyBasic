2b053d3774663b78c8ecdedc9442c82030f04536
zkr5471 <mrzkr2ds@gmail.com>
2021-05-06 04:46:55 +0900

    optimize

--- src/OptimizeExpr.cpp
+++ src/OptimizeExpr.cpp
@@ -17,12 +17,16 @@ public:
   {
   }
 
-  static SignedExpr FromExprRight(AST::Expr* expr) {
+  static SignedExpr FromExprRight(AST::Expr* expr)
+  {
     auto right = expr->right;
-    int sign = (expr->type == AST::Expr::Add ? 1 : -1); // default sign (expr->type)
-    if( right->type == AST::Expr::Immidiate ) {
+    auto sign = (expr->type == AST::Expr::Add ? 1 : -1); // default sign (expr->type)
+    
+    if( right->type == AST::Expr::Immidiate )
+    {
       sign *= right->token->obj.v_int > 0 ? 1 : -1; // if immidiate is negative, invert sign
     }
+    
     return SignedExpr(sign, expr->right);
   }
 };
@@ -50,8 +54,10 @@ void AST::Expr::Optimize()
     // remove immidiate! (to immidiatePart)
     Expr* immidiatePart = new Expr();
     immidiatePart->token = new Token();
-    for( auto it = terms.begin(); it != terms.end(); ) {
-      if( it->expr->type == Immidiate ) {
+    for( auto it = terms.begin(); it != terms.end(); )
+    {
+      if( it->expr->type == Immidiate )
+      {
         immidiatePart->token->obj.v_int += it->expr->token->obj.v_int * it->sign;
         terms.erase(it);
       }
