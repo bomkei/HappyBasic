0fa9dbb19dfc5509f3917c1f35f6cb8d1568414b
zkr5471 <mrzkr2ds@gmail.com>
2021-05-05 23:06:16 +0900

    reduce AST source files

--- src/AST_ToString.cpp
+++ src/AST_ToString.cpp
@@ -69,3 +69,81 @@ std::string AST::Expr::ToString() const
 
   return "";
 }
+
+bool AST::Expr::IsConstexpr() const
+{
+  switch( type )
+  {
+  case Immidiate:
+    return true;
+
+  default:
+    if( !left )
+      return right ? right->IsConstexpr() : false;
+    else if( !right )
+      return left ? left->IsConstexpr() : false;
+
+    return left->IsConstexpr() && right->IsConstexpr();
+  }
+  
+  return false;
+}
+
+bool AST::Expr::operator != (AST::Expr const& ast)const
+{
+  return (*this == ast) == false;
+}
+
+bool AST::Expr::operator == (AST::Expr const& _ast) const
+{
+  auto ast = (Expr*)&_ast;
+
+  if( !ast )
+    return false;
+
+  if( ast->type != type )
+    return false;
+
+  std::cout << ToString() << ", " << ast->ToString() << '\n';
+
+  switch( type )
+  {
+  case Immidiate:
+    return token->obj.equal(ast->token->obj);
+
+  case Variable:
+    return varIndex == ast->varIndex;
+
+  case Callfunc:
+  {
+    if( token->str != ast->token->str )
+      return false;
+
+    if( ((AST::Callfunc*)this)->args.size() != ((AST::Callfunc*)ast)->args.size() )
+      return false;
+
+    for( size_t i = 0; i < ((AST::Callfunc*)this)->args.size(); i++ )
+      if( *((AST::Callfunc*)this)->args[i] != *((AST::Callfunc*)ast)->args[i] )
+        return false;
+
+    return true;
+  }
+
+  case Array:
+  {
+    if( ((AST::Array*)this)->elems.size() != ((AST::Array*)ast)->elems.size() )
+      return false;
+
+    for( size_t i = 0; i < ((AST::Array*)this)->elems.size(); i++ )
+      if( *((AST::Array*)this)->elems[i] != *((AST::Array*)ast)->elems[i] )
+        return false;
+
+    return true;
+  }
+
+  default:
+    return *left == *ast->left && *right == *ast->right;
+  }
+
+  return false;
+}
