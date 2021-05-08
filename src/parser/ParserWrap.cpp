255b929ca0edac62bfad6c4055482967f0a5599f
zkr5471 <mrzkr2ds@gmail.com>
2021-05-08 15:05:14 +0900

    wrap

--- src/parser/ParserWrap.cpp
+++ src/parser/ParserWrap.cpp
@@ -4,7 +4,30 @@ AST::Block* ParserWrap::Parse()
 {
   auto ast = new AST::Block;
 
-  
+  while( check() )
+  {
+    if( match("if") )
+      ast->list.emplace_back(If());
+
+    else if( match("for") )
+      ast->list.emplace_back(For());
+
+    else if( match("while") )
+      ast->list.emplace_back(While());
+
+    else if( match("do") )
+      ast->list.emplace_back(DoWhile());
+
+    else if( match("def") )
+      ast->list.emplace_back(Define());
+
+    else if( match("class") )
+      ast->list.emplace_back(Class());
+
+    else
+      ast->list.emplace_back(Statements());
+
+  }
 
   return ast;
 }
