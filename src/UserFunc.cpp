eeea546a40035abd2601132211a8ca2b36a52b3e
zkr5471 <mrzkr2ds@gmail.com>
2021-05-09 09:52:35 +0900

    if( Options::IsSafety

--- src/UserFunc.cpp
+++ src/UserFunc.cpp
@@ -36,9 +36,9 @@ Object AST_Runner::UserFunc(AST::Callfunc* fun)
   
   CallCount++;
 
-  if( CallCount >= FUNC_CALL_DEPTH_MAX )
+  if( Options::IsSafety && CallCount >= FUNC_CALL_DEPTH_MAX )
   {
-    Program::Error(*fun->token, "prevented crash of stack overflow. but you can suppression with option.");
+    Program::Error(*fun->token, "prevented crash of stack overflow.");
   }
 
   //
