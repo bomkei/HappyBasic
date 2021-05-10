521f707709dc47ab949e5615b5f3e4b0fdce76c9
zkr5471 <mrzkr2ds@gmail.com>
2021-05-11 02:39:36 +0900

    impl parse of class

--- src/parser/Class.cpp
+++ src/parser/Class.cpp
@@ -8,7 +8,34 @@ Object make_new_class_Obj(std::string& name)
 AST::Class *ParserCore::Class()
 {
   auto ast = new AST::Class;
+  ast->token = csmtok;
 
+  auto closed = false;
 
+  expect("\n");
+
+  while( check() ) {
+    if( consume("End") ) {
+      expect("Class");
+      expect("\n");
+      closed = true;
+      break;
+    }
+
+    auto tk = &get_tok(); {
+      if( tk->str != "Var" &&
+        tk->str != "Func" &&
+        tk->str != "Class" ) {
+        Program::Error(*tk, "at9032j0t2j40");
+      }
+    }
+
+    ast->member_list.emplace_back(Statements());
+  }
+
+  if( !closed )
+    Program::Error(*ast->token, "not closed");
+
+  return ast;
 }
 
