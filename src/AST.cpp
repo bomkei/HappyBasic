c39e0a9e16e9ab8bcd5f444cb90c94fa451da008
syoch <hacksyoch@gmail.com>
2021-05-12 00:34:09 +0900

    add ctor

--- src/AST.cpp
+++ src/AST.cpp
@@ -1,15 +1,31 @@
 #include "main.h"
 
 AST::Expr::Expr(Type type)
-  :type(type), left(nullptr), right(nullptr), token(nullptr)
+    : type(type)
+    , left(nullptr)
+    , right(nullptr)
+    , token(nullptr)
 {
 }
 
 AST::Expr::Expr(Type type, Expr* left, Expr* right, Token* tok)
-  : type(type), left(left), right(right), token(tok)
+    : type(type)
+    , left(left)
+    , right(right)
+    , token(tok)
 {
 }
 
+AST::Expr::Expr(float flt)
+    : type(Immidiate)
+    , left(nullptr)
+    , right(nullptr)
+    , token(nullptr)
+{
+  token = new Token();
+  token->obj.type = Object::Float;
+  token->obj.v_float = flt;
+}
 
 std::string AST::Expr::ToString() const
 {
@@ -30,7 +46,8 @@ std::string AST::Expr::ToString() const
       for( int i = 0; i < elems.size(); i++ )
       {
         s += elems[i]->ToString();
-        if( i < elems.size() - 1 ) s += ", ";
+        if( i < elems.size() - 1 )
+          s += ", ";
       }
 
       return "[" + s + "]";
@@ -44,7 +61,8 @@ std::string AST::Expr::ToString() const
       for( int i = 0; i < args.size(); i++ )
       {
         s += args[i]->ToString();
-        if( i < args.size() - 1 ) s += ", ";
+        if( i < args.size() - 1 )
+          s += ", ";
       }
 
       return token->str + "(" + s + ")";
@@ -69,22 +87,23 @@ std::string AST::Expr::ToString() const
       auto right = this->right->ToString();
 
       return "(" + left + ")" +
-        [] (Type t) {
-        switch( t ) {
-          case Add: return "+";
-          case Sub: return "-";
-          case Mul: return "*";
-          case Div: return "/";
-          case Shift: return "<<";
-          case Bigger: return ">";
-          case BiggerOrEqual: return ">=";
-          case Equal: return "==";
-          case NotEqual: return "!=";
-        }
-        return "?";
-      }(this->type) + right;
+          [](Type t) {
+            switch( t )
+            {
+              case Add: return "+";
+              case Sub: return "-";
+              case Mul: return "*";
+              case Div: return "/";
+              case Shift: return "<<";
+              case Bigger: return ">";
+              case BiggerOrEqual: return ">=";
+              case Equal: return "==";
+              case NotEqual: return "!=";
+            }
+            return "?";
+          }(this->type)
+          + right;
     }
-
   }
 
   return "";
@@ -99,7 +118,8 @@ bool AST::Expr::IsConstexpr() const
 
     case Array:
       for( auto&& i : ((AST::Array*)this)->elems )
-        if( !i->IsConstexpr() ) return false;
+        if( !i->IsConstexpr() )
+          return false;
       return true;
 
     case Variable:
@@ -108,11 +128,13 @@ bool AST::Expr::IsConstexpr() const
     default:
       if( !left )
       {
-        if( right ) return right->IsConstexpr();
+        if( right )
+          return right->IsConstexpr();
       }
       else if( !right )
       {
-        if( left ) return left->IsConstexpr();
+        if( left )
+          return left->IsConstexpr();
       }
 
       return left->IsConstexpr() && right->IsConstexpr();
@@ -182,40 +204,34 @@ std::string AST::Stmt::ToString(int tab) const
 
       for( auto&& p : ((AST::If*)this)->pairs )
       {
-        s +=
-          " " + std::get<0>(p)->ToString() + ":\n" +
-          " " + std::get<1>(p)->ToString(tab + 1);
+        s += " " + std::get<0>(p)->ToString() + ":\n" + " " + std::get<1>(p)->ToString(tab + 1);
       }
 
       return s + "\n";
     }
 
     case Type::For:
-      return
-        "for " + ((AST::For*)this)->counter->ToString() + " = " + ((AST::For*)this)->begin->ToString() +
-        " to " + ((AST::For*)this)->end->ToString() + "\n" +
-        ((AST::For*)this)->code->ToString(tab + 1) + "next\n";
+      return "for " + ((AST::For*)this)->counter->ToString() + " = " + ((AST::For*)this)->begin->ToString() + " to " + ((AST::For*)this)->end->ToString() + "\n" + ((AST::For*)this)->code->ToString(tab + 1) + "next\n";
 
     case Type::While:
-      return
-        "while " + ((AST::While*)this)->cond->ToString() + "\n"
-        + ((AST::While*)this)->code->ToString(tab + 1) + "wend\n";
+      return "while " + ((AST::While*)this)->cond->ToString() + "\n"
+          + ((AST::While*)this)->code->ToString(tab + 1) + "wend\n";
 
-    //case Type::Assign:
-    //  return std::string(tab, ' ') + ((AST::Assign*)this)->var->ToString() + " = " + ((AST::Assign*)this)->value->ToString() + "\n";
+      //case Type::Assign:
+      //  return std::string(tab, ' ') + ((AST::Assign*)this)->var->ToString() + " = " + ((AST::Assign*)this)->value->ToString() + "\n";
 
-    //case Type::Instruction:
-    //{
-    //  std::string s = std::string(tab, ' ') + ((AST::Instruction*)this)->name + " ";
+      //case Type::Instruction:
+      //{
+      //  std::string s = std::string(tab, ' ') + ((AST::Instruction*)this)->name + " ";
 
-    //  for( size_t i = 0; i < ((AST::Instruction*)this)->args.size(); i++ )
-    //  {
-    //    s += ((AST::Instruction*)this)->args[i]->ToString();
-    //    if( i < ((AST::Instruction*)this)->args.size() - 1 ) s += ", ";
-    //  }
+      //  for( size_t i = 0; i < ((AST::Instruction*)this)->args.size(); i++ )
+      //  {
+      //    s += ((AST::Instruction*)this)->args[i]->ToString();
+      //    if( i < ((AST::Instruction*)this)->args.size() - 1 ) s += ", ";
+      //  }
 
-    //  return s + "\n";
-    //}
+      //  return s + "\n";
+      //}
 
     case Type::Block:
     {
