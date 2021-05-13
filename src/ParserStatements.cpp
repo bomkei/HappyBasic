d21ab7ae3733d684d49326de92c221ba97c5ea5a
zkr5471 <mrzkr2ds@gmail.com>
2021-05-13 19:23:58 +0900

    remake parse code of do-while

--- src/ParserStatements.cpp
+++ src/ParserStatements.cpp
@@ -85,46 +85,23 @@ AST::Stmt* ParserCore::Statements() {
   }
 
   if( consume("do") ) {
-    std::vector<AST::Stmt*> list;
-    AST::Expr* cond = nullptr;
-    auto closed = false;
-
-    while( check() ) {
-      if( consume("while") ) {
-        cond = Expr();
-        expect("\n");
-        closed = true;
-        break;
-      }
-
-      list.emplace_back(Statements());
-    }
-
     auto ast = new AST::While;
-
     ast->cond = AST::Expr::FromInt(1);
+
     ast->code = new AST::Block;
+    ast->code->list.emplace_back(Statements());
+    
+    auto cmp = new AST::If;
+    expect("while");
+    expect("(");
+    cmp->cond = Expr();
+    expect(")");
+    expect(";");
 
-    {
-      auto bb = new AST::Block;
-      bb->list = std::move(list);
-      ast->code->list.emplace_back(bb);
-    }
+    cmp->code = new AST::Stmt;
+    cmp->code->type = AST::Stmt::Break;
 
-    {
-      auto iff = new AST::If;
-
-      {
-        auto breakk = new AST::Block; {
-          auto bruh = new AST::Stmt;
-          bruh->type = AST::Stmt::Break;
-          breakk->list.emplace_back(bruh);
-        }
-
-        iff->pairs.emplace_back(std::make_pair(cond, breakk));
-      }
-      ast->code->list.emplace_back(iff);
-    }
+    ast->code->list.emplace_back(cmp);
 
     return ast;
   }
