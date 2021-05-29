06a33087983bbac241b3952c452c1872d620a394
zkr5471 <mrzkr2ds@gmail.com>
2021-05-29 10:08:39 +0900

    dont make variable automaticallly when parse

--- HappyBasic_VS/Parser.cpp
+++ HappyBasic_VS/Parser.cpp
@@ -243,12 +243,12 @@ namespace ParserCore {
         ast->type = AST::Expr::Variable;
         ast->token = tok;
 
-        if( !DontPlace_v ) {
-          if( (ast->varIndex = find_var(tok->str)) == -1 ) {
-            ast->varIndex = variables.size();
-            variables.emplace_back(tok->obj);
-          }
-        }
+        //if( !DontPlace_v ) {
+        //  if( (ast->varIndex = find_var(tok->str)) == -1 ) {
+        //    ast->varIndex = variables.size();
+        //    variables.emplace_back(tok->obj);
+        //  }
+        //}
 
         return ast;
       }
